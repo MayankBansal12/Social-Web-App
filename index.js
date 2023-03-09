8aa045c23bee7b5c34304a50fdb2c2dc6dcd311b
Mayank <mayankbansal125@gmail.com>
2023-03-09 22:57:23 +0530

    Added the layouts i.e., header and footer

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require("express");
 const app=express();
 const port=8000;
 
+const expressLayouts=require("express-ejs-layouts");
+app.use(expressLayouts);
+
 // Set up Router Engine
 app.use('/',require('./routes'));
 
